996c2d0e1457e4f3c2b06e423b47c895ea9fd011
kenna.hwa <kenna.hwa@gmail.com>
2021-05-28 15:15:18 +0900

    join form 수정

---
+++ mobile/js/index.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+
+
+
+    // 변수 선언
+    let youtubeLink = document.querySelector(".youtube_official")
+
+
+
+    // DOM
+
+
+
+
+    // 함수
+    document.addEventListener("click", function)
+
+
+};
