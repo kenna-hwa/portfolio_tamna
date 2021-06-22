354712405939da0e7b66fc770a47040d1f1583ef
kenna.hwa <kenna.hwa@gmail.com>
2021-06-22 18:21:35 +0900

    join form action 전송

--- join3.php
+++ join3.php
@@ -166,13 +166,13 @@ $usermobile = $_POST["phoneNum"];
     <!-- footer start -->
     <footer id="footer" class="footer">
         <div class="footer_box">
-            <h2><a href="#">탐나는전 업체 정보</a></h2>
+            <h2><a href="http://tamna.jeju.go.kr">탐나는전 업체 정보</a></h2>
             <div class="footer_txt">
                 <address>주소 : (63122) 제주특별자치도 제주시 문연로 6(연동)</address>
                 <p>대표전화 : 1600-3971 (09:00 ~ 18:00)</p>
                 <p>Copyright 2020 제주특별자치도. All rights reserved</p>
             </div>
-            <div class="footer_jejugoverment"><a href="#">제주특별자치도 바로가기</a></div>
+            <div class="footer_jejugoverment"><a href="http://www.jeju.go.kr">제주특별자치도 바로가기</a></div>
         </div>
     </footer>
 
