a19b26603c7d717563ec5f78b9049df15b40d5f4
kenna.hwa <kenna.hwa@gmail.com>
2021-06-22 10:46:08 +0900

    파일명 정리

--- js/index.js
+++ js/index.js
@@ -1,4 +1,27 @@
+$(function(){
+    //gnb
+    $(".topmenu").mouseenter(function () {
+        $(".sub, .gnb_sub_box").stop().slideDown("1200");
+    });
+    $(".main").mouseenter(function () {
+        $(".sub, .gnb_sub_box").stop().slideUp("1000");
+    });
 
+    //이미지 캐러셀
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoDelay: 0,
+        speed: 1850,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1600,
+        slideMargin: 0,
+        infiniteLoop: true,
+        touchEnabled: true,
+        autoHover: true
+    });
+  });
 
 window.onload = function(){
 
