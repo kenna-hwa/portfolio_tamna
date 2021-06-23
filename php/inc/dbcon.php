fca42f43f559e0874e072b95c28c4aed2b849fb7
kenna.hwa <kenna.hwa@gmail.com>
2021-06-23 11:58:25 +0900

    dbcon.php 파일 수정

--- php/inc/dbcon.php
+++ php/inc/dbcon.php
@@ -1,10 +1,10 @@
 
 
-<?php
-//DB 연결
-$dbcon = mysqli_connect("localhost", "root", "", "front") or die("DB 접속 실패");
-mysqli_set_charset($dbcon, "utf8");
+    <?php
+    //DB 연결
+    $dbcon = mysqli_connect("112.175.184.73", "kenna", "peacehwa91!", "kenna") or die("DB 접속 실패");
+    mysqli_set_charset($dbcon, "utf8");
 
 
 
-?>
+    ?>
