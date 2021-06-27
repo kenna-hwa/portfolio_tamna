f95f87c3925659d8e4abdb368791d362bcfa1c00
kenna.hwa <kenna.hwa@gmail.com>
2021-06-27 22:15:34 +0900

    footer.css 배경색 추가 , footer.php 링크 수정

--- php/footer.php
+++ php/footer.php
@@ -2,13 +2,13 @@
 
         <footer id="footer" class="footer">
             <div class="footer_box">
-                <h2><a href="#">탐나는전 업체 정보</a></h2>
+                <h2><a href="kenna.dothome.co.kr">탐나는전 업체 정보</a></h2>
                 <div class="footer_txt">
                     <address>주소 : (63122) 제주특별자치도 제주시 문연로 6(연동)</address>
                     <p>대표전화 : 1600-3971 (09:00 ~ 18:00)</p>
                     <p>Copyright 2020 제주특별자치도. All rights reserved</p>
                 </div>
-                <div class="footer_jejugoverment"><a href="#">제주특별자치도 바로가기</a></div>
+                <div class="footer_jejugoverment"><a href="https://www.jeju.go.kr/index.htm">제주특별자치도 바로가기</a></div>
             </div>
         </footer>
 
