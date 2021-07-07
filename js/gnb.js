baaf02f4bf964c767d6cc86ed8eecd2dfb8340e7
kenna.hwa <kenna.hwa@gmail.com>
2021-07-08 01:36:39 +0900

    모바일 index 완성

--- js/gnb.js
+++ js/gnb.js
@@ -2,11 +2,14 @@ $(function(){
     $(".sub, .gnb_sub_box").css('display','none');
     //gnb
 
-    $(".topmenu, .sub, .gnb_sub_box").mouseenter(function () {
+    $(".header_box, .topmenu, .sub, .gnb_sub_box").mouseenter(function () {
         $(".sub, .gnb_sub_box").stop().slideDown("1200");
     });
-    $(".topmenu, .sub, .gnb_sub_box").mouseleave(function () {
+    $(".header_box, .topmenu, .sub, .gnb_sub_box").mouseleave(function () {
         $(".sub, .gnb_sub_box").stop().slideUp("1000");
     });
 
 });
+
+
+
