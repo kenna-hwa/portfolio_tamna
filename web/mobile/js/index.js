cdc4507fdd783d3855c1c6edf7977a9347363e23
kenna.hwa <kenna.hwa@gmail.com>
2021-07-15 02:00:40 +0900

    랜딩페이지 레이아웃 완성

--- web/mobile/js/index.js
+++ web/mobile/js/index.js
@@ -1,18 +1,16 @@
+//이미지 슬라이드
+
 var ul = document.querySelector('.slidewrap');
 
 var firstItemClone = ul.firstElementChild.cloneNode(true);
         ul.appendChild(firstItemClone);
         
 function move() {
-
     var curIndex = 0;
-
     setInterval(function () {
         ul.style.transition = '3s';
         ul.style.transform = "translate3d(-" + 767 * (curIndex + 1) + "px, 0px, 0px)";
-
         curIndex++;
-
         if (curIndex === 4) {
             setTimeout(function () {
                 ul.style.transition = '0s';
@@ -20,9 +18,9 @@ function move() {
             }, 20)
             curIndex = 0;
         }
-
     }, 3800);
 };
 document.addEventListener("DOMContentLoaded", function () {
     move();
 });
+
